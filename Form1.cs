0df68839711bdea2b21556e55f95bc9344f249fc
ulianaUlti <124128505+ulianaUlti@users.noreply.github.com>
2023-05-10 10:45:07 +0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Практическая_15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if(textBox1.Text==""|| textBox2.Text == "")
+                {
+                  MessageBox.Show("Пустые поля");
+                }
+
+            }
+            catch
+            {
+                MessageBox.Show("Неверный ввод");
+            }
+            Number num1 = new Number(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text));
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text == "" || textBox2.Text == "")
+                {
+                    MessageBox.Show("Пустые поля");
+                }
+
+            }
+            catch
+            {
+                MessageBox.Show("Неверный ввод");
+            }
+            Number num1 = new Number(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text == "" || textBox2.Text == "")
+                {
+                    MessageBox.Show("Пустые поля");
+                }
+
+            }
+            catch
+            {
+                MessageBox.Show("Неверный ввод");
+            }
+            Number num1 = new Number(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text));
+
+        }
+    }
+}
---
+++ Numbers.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практическая_15
+{
+    class Number:Form1
+    {
+        public int A;
+        public int BJ;
+        public int C;
+        public int DJ;
+        private int v;
+        private int v1;
+        private int v2;
+
+        public Number(int v)
+        {
+            this.v = v;
+        }
+
+        public Number(int v, int v1, int v2) : this(v)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+
+        public Number(int a, int bj, int c, int dj)
+        {
+            A = a;
+            BJ = bj;
+            C = c;
+            DJ = dj;
+
+        }
+
+   
+        public static Number operator +(Number num1, Number num2)
+        {
+            return new Number((num1.A + num2.BJ) + (num1.C + num2.DJ));
+        }
+        public static Number operator -(Number num1, Number num2)
+        {
+            return new Number((num1.A - num2.BJ) - (num1.C - num2.DJ));
+        }
+        public static Number operator *(Number num1, Number num2)
+        {
+            return new Number((num1.A + num2.DJ) * (num1.C + num2.DJ));
+        }
+    }
+ }
+
+
+
+    
+
+
